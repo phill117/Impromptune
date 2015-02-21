4e2bce54878be297cd987b4451280fa86dd07a68
Jacob Richwine <jrichwin@purdue.edu>
2015-02-21 16:25:31 -0500

    First Renderer Branch Commit

--- Impromptune/src/sample/Controller.java
+++ Impromptune/src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 public class Controller {
+   // private MenuItem zong;
+
+
+
+
 }
--- Impromptune/src/sample/Main.java
+++ Impromptune/src/sample/Main.java
@@ -1,22 +1,33 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.MenuItem;
 
 public class Main extends Application {
+
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World - No hablo espanol");
         primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.setFullScreen(true);
+      //  primaryStage.setFullScreen(true); ANNOYING AS HELL!
         primaryStage.show();
     }
 
+    @FXML
+    void zong(ActionEvent event) {
+        System.out.println("HELLO");
+        System.exit(0);
+    }
 
     public static void main(String[] args) {
         launch(args);
