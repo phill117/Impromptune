c3af0490b9fb0c37d0f2a839efbcfdf2d19032ea
Chris Doak <doak@purdue.edu>
2015-02-25 23:12:51 -0500

    Created a layout for the main view.

---
+++ Impromptune/src/impromptune_gui/Impromptune.java
@@ -0,0 +1,18 @@
+package impromptune_gui;/**
+ * Created by cdoak_000 on 2/25/2015.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Impromptune extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
