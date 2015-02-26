f78c97014711ba8591b234e1127c660d0c8941a2
Chris Doak <doak@purdue.edu>
2015-02-26 09:44:09 -0500

    Revised the MainView to be a base layout. Made an edit view. (subject to change)

--- Impromptune/src/impromptune_gui/Impromptune.java
+++ Impromptune/src/impromptune_gui/Impromptune.java
@@ -1,18 +1,25 @@
-package impromptune_gui;/**
- * Created by cdoak_000 on 2/25/2015.
+package impromptune_gui;
+/**
+ * Created by doak on 2/25/2015.
  */
 
 import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Impromptune extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private Stage primaryStage;
+    private BorderPane baseLayout;
+    private AnchorPane editView;
 
     @Override
     public void start(Stage primaryStage) {
 
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
