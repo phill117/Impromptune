fd9be651eedca70b3423424ed6950d9ac9b7af22
cdoak94 <doak@purdue.edu>
2015-03-02 22:58:11 -0500

    Hammered out more of the Main App GUI

--- Impromptune/src/impromptune_gui/Impromptune.java
+++ Impromptune/src/impromptune_gui/Impromptune.java
@@ -4,18 +4,69 @@ package impromptune_gui;
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.io.IOException;
+import piano.PianoHolder;
 
 public class Impromptune extends Application {
 
     private Stage primaryStage;
     private BorderPane baseLayout;
-    private AnchorPane editView;
+    private BorderPane editView;
+    private PianoHolder piano;
 
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Impromptune");
+
+        initBaseLayout();
+        showEditView();
+        initPiano();
+    }
+
+    /**
+     * Initializes the base layout
+     */
+    public void initBaseLayout() {
+        try {
+            // Load base layout from fxml file
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Impromptune.class.getResource("MenuBarBaseLayout.fxml"));
+            baseLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the base layout
+            Scene scene = new Scene(baseLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the edit view in the base layout.
+     */
+    public void showEditView() {
+        try {
+            // Load edit view
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Impromptune.class.getResource("EditView.fxml"));
+            editView = (BorderPane) loader.load();
+
+            // Set edit view to center of base layout
+            baseLayout.setCenter(editView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initPiano() {
 
     }
 
