ac79bed9aa3ddeef39b410e566de5d140cfae1a8
Jacob Richwine <jrichwin@purdue.edu>
2015-03-03 23:36:17 -0500

    Started Renderer Module

---
+++ Impromptune/src/Renderer/MainWindow.java
@@ -0,0 +1,53 @@
+package Renderer;
+
+//import java.awt.image.BufferedImage;
+
+
+import Renderer.Content;
+import Renderer.Playback;
+import com.xenoage.zong.layout.Layout;
+import com.xenoage.zong.renderer.javafx.JfxLayoutRenderer;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import org.controlsfx.dialog.Dialog;
+import org.controlsfx.dialog.Dialogs;
+
+//import com.xenoage.zong.renderer.awt.AwtLayoutRenderer;
+
+
+/**
+ * Controller for the JavaFX main window (MainWindow.fxml).
+ * 
+ * @author Andreas Wenger
+ */
+public class MainWindow {
+
+	//GUI elements
+	@FXML private BorderPane pnlCanvas;
+	@FXML private ImageView scoreView;
+
+	//loaded content
+	private Content content = new Content(this);
+	private WritableImage scoreImage = null;
+
+	@FXML public void initialize() {
+		//load the first demo score
+		content.loadNextScore();
+	}
+
+	public void renderLayout(Layout layout) {
+		//run in JavaFX application thread
+		Platform.runLater(() -> {
+			scoreImage = JfxLayoutRenderer.paintToImage(layout, 0, 2f);
+			scoreView.setImage(scoreImage);
+			scoreView.setFitWidth(scoreImage.getWidth());
+			scoreView.setFitHeight(scoreImage.getHeight());
+		});
+	}
+
+}
--- Impromptune/src/impromptune_gui/ImpromptuneInitializer.java
+++ Impromptune/src/impromptune_gui/ImpromptuneInitializer.java
@@ -1,12 +1,6 @@
 package impromptune_gui;
 
-import com.xenoage.utils.error.Err;
-import com.xenoage.utils.jse.log.DesktopLogProcessing;
-import com.xenoage.utils.log.Log;
-import com.xenoage.zong.demos.simplegui.SimpleGuiDemo;
-import com.xenoage.zong.desktop.io.midi.out.SynthManager;
 import com.xenoage.zong.desktop.utils.JseZongPlatformUtils;
-import com.xenoage.zong.desktop.utils.error.GuiErrorProcessing;
 import com.xenoage.zong.gui.PlayerFrame;
 import com.xenoage.zong.player.Player;
 import javafx.fxml.FXML;
@@ -14,8 +8,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.AnchorPane;
-
-import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -32,10 +24,7 @@ public class ImpromptuneInitializer implements Initializable{
     Player player = null;
     static PlayerFrame frame = null;
 
-    SimpleGuiDemo r = null;
-
     public static final String appName = "Impromptune";
-    public static final String appVersion = ".01";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -50,11 +39,11 @@ public class ImpromptuneInitializer implements Initializable{
             PlayerCase.getChildren().add(frame.getRoot());
 
             //Renderer
-            JseZongPlatformUtils.init(appName);
+            JseZongPlatformUtils.init(appName); // JUST GOTTA DO IT MAN!!!
          //   Log.init(new DesktopLogProcessing(appName + " " + appVersion));
           //  Err.init(new GuiErrorProcessing());
           //  SynthManager.init(false)
-           RendererCase.getChildren().add(FXMLLoader.load(getClass().getResource("Renderer.fxml")));
+           RendererCase.getChildren().add(FXMLLoader.load(getClass().getClassLoader().getResource("Renderer/Renderer.fxml")));
 
         }catch (Exception e){
             System.out.println("WAHT HAPPENED");
