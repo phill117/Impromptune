04615948ab758b2d98bb8297af172c08a60d9ccb
Chris Doak <doak@purdue.edu>
2015-03-31 12:55:35 -0400

    Added the beginnings of a Composition Properties dialog to handle all of the composition properties.

---
+++ Impromptune/src/impromptune_gui/Dialogs/CompositionPropertiesDialog.java
@@ -0,0 +1,21 @@
+package impromptune_gui.Dialogs;
+
+import javafx.fxml.*;
+import com.xenoage.utils.jse.javafx.Dialog;
+
+/**
+ * Created by cdoak_000 on 3/31/2015.
+ * This is for the dialog box that allows the user to choose:
+ * Title
+ * Author
+ * Tempo
+ * Key Signature
+ * etc.
+ * of the composition
+ */
+
+public class CompositionPropertiesDialog extends Dialog {
+    public void initialize() {
+
+    }
+}
