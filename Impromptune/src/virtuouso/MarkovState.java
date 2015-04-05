e21c259eabe3720eba79727cf478e8dbfd2cf0d7
Ben Ahlbrand <bmahlbrand@gmail.com>
2015-04-05 15:44:37 -0400

    cleaned up implementation

---
+++ Impromptune/src/virtuouso/MarkovState.java
@@ -0,0 +1,113 @@
+package virtuouso;
+
+/**
+ * Created by ben on 4/5/2015.
+ */
+public class MarkovState {
+    private PitchAxis pitchAxis;
+    private double probMatrix[][];
+    private int histogram[][];
+
+    public MarkovState() {
+        this.histogram = new int [12][12];
+        this.probMatrix = new double [12][12];
+        this.pitchAxis = new PitchAxis();
+    }
+
+    private void markIndexFound(int i, int j) {
+        histogram[i][j]++;
+    }
+
+    public void updateLayer(String currentPitch, String lastPitch) {
+        markIndexFound(pitchAxis.getIndex(currentPitch), pitchAxis.getIndex(lastPitch));
+    }
+
+    public void printStateHistogram() {
+        System.out.println("-----------------------------------------");
+//        System.out.println("total notes in model: " + statesCounter + " of order: " + k);
+
+        System.out.printf("    ");
+        String[] pitches = pitchAxis.getPitchAxis();
+        for (String s : pitches) {
+            if (s.length() == 2)
+                System.out.printf(" %s", s);
+            else
+                System.out.printf(" %s ", s);
+        }
+
+        System.out.printf(" sums\n");
+
+        for (int i = 0; i < histogram.length; i++) {
+            int sum = 0;
+
+            if (pitches[i].length() == 2)
+                System.out.printf(" %s ", pitches[i]);
+            else
+                System.out.printf(" %s  ", pitches[i]);
+
+            for (int j = 0; j < histogram[0].length; j++) {
+                System.out.printf(" %d,", histogram[i][j]);
+                sum += histogram[i][j];
+            }
+
+            System.out.printf(" " + sum + "\n");
+        }
+
+    }
+
+    public void printStateProbMatrix() {
+        normalize();
+
+        System.out.printf("    ");
+        String[] pitches = pitchAxis.getPitchAxis();
+        for (String s : pitches) {
+            if (s.length() == 2)
+                System.out.printf(" %s  ", s);
+            else
+                System.out.printf(" %s   ", s);
+        }
+
+        System.out.printf("\n");
+
+        for (int i = 0; i < probMatrix.length; i++) {
+            if (pitches[i].length() == 2)
+                System.out.printf(" %s  ", pitches[i]);
+            else
+                System.out.printf(" %s   ", pitches[i]);
+
+            for (int j = 0; j < probMatrix[0].length; j++) {
+                System.out.printf("%.2f,", probMatrix[i][j]);
+            }
+            System.out.printf("\n");
+        }
+    }
+
+    public void normalize() {
+
+        for (int i = 0; i < 12; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < 12; j++) {
+                sum += histogram[i][j];
+            }
+
+            double likelihood = 1.0; //start with 100%
+            if (sum == 0)
+                likelihood = 0;
+            else
+                likelihood /= sum;
+
+            for (int j = 0; j < 12; j++) {
+                probMatrix[i][j] = likelihood * histogram[i][j];
+            }
+        }
+
+        for (int i = 0; i < 12; i++) {
+            double val = 0.0;
+
+            for (int j = 0; j < 12; j++)
+                val += probMatrix[i][j];
+
+        }
+    }
+}
--- Impromptune/src/virtuouso/ToneTransitionTable.java
+++ Impromptune/src/virtuouso/ToneTransitionTable.java
@@ -2,6 +2,7 @@ package virtuouso;
 
 import utils.LimitedQueue;
 import utils.MersenneTwisterFast;
+import utils.Pair;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -13,47 +14,36 @@ import java.util.LinkedList;
 public class ToneTransitionTable {
 
     public static void main (String args[]) {
-        ToneTransitionTable ttt = new ToneTransitionTable(1);
+        ToneTransitionTable ttt = new ToneTransitionTable(2);
 
-        for (int i = 0; i < 144; i++) {
-            ttt.generateNextState(BlackMagicka.noteIndexToString(ttt.rand.nextInt(12)));
+        for (int i = 0; i < 1444; i++) {
+            ttt.generateNextState(BlackMagicka.noteIndexToString(ttt.getRand(12)));
         }
 
         ttt.printHistogram();
         ttt.printProbMatrix();
     }
 
-    private double probMatrix[][] = {};
-    public int counts[][] = {};
+    private MersenneTwisterFast rand = new MersenneTwisterFast();
 
-    private PitchAxis axis = null;
-
-    MersenneTwisterFast rand = new MersenneTwisterFast();
-
-    private int order; //default
+    private int order;
     private int statesCounter = 0;
 
-    private ArrayList<int [][]> markov = null;
-    private LinkedList<String> lastKnotes = null;
+    private LinkedList<String> lastKnotes;
+    private LinkedList<MarkovState> markov;
 
     public ToneTransitionTable(int order) {
-        this.axis = new PitchAxis();
         this.order = order;
-        this.counts = new int [12][12];
-        this.probMatrix = new double[12][12];
         this.lastKnotes = new LimitedQueue<>(order);
-        this.markov = new ArrayList<>();
+        this.markov = new LimitedQueue<>(order);
 
         int i = 0;
         while (i++ < order) //add k dimensions for model
-            markov.add(counts);
-
+            markov.add(new MarkovState());
     }
 
     public void generateNextState(String currentPitch) {
         statesCounter++;
-
-        String lastPitch = lastKnotes.peekFirst();
 
         if (lastKnotes.size() == 0) {
             lastKnotes.add(currentPitch);
@@ -66,97 +56,26 @@ public class ToneTransitionTable {
 
     private void updateKOrderLayers(String currentPitch) {
         for (int i = 0; i < lastKnotes.size(); i++)
-            markIndexFound(axis.getIndex(lastKnotes.get(i)),
-                    axis.getIndex(currentPitch),
-                    i);
+            markov.get(i).updateLayer(currentPitch, lastKnotes.get(i));
     }
 
-    private void markIndexFound(int i, int j, int k) { //i = m, j = n, k = constant
-        markov.get(k)[i][j]++;
-    }
-
-    public void normalize() {
-
-        for (int i = 0; i < 12; i++) {
-            int sum = 0;
-
-            for (int j = 0; j < 12; j++) {
-                sum += counts[i][j];
-            }
-
-            double likelihood = 1.0 / sum;
-
-            for (int j = 0; j < 12; j++) {
-                probMatrix[i][j] = likelihood * counts[i][j];
-            }
-        }
-
-        for (int i = 0; i < 12; i++) {
-            double val = 0.0;
-
-            for (int j = 0; j < 12; j++)
-                val += probMatrix[i][j];
-
-//            System.out.println(val);
-
-        }
-
-//        return probMatrix;
-    }
-
-    public void clear() {
-        this.probMatrix = new double[order][order];
+    private int getRand(int i) {
+        return rand.nextInt(i);
     }
 
     public void printHistogram() {
-        System.out.printf("    ");
-        String [] pitches = axis.getPitchAxis();
-        for (String s : pitches) {
-            if (s.length() == 2)
-                System.out.printf(" %s", s);
-            else
-                System.out.printf(" %s ", s);
-        }
-
-        System.out.printf("\n");
-
-        for (int i = 0; i < counts.length; i++) {
-            if (pitches[i].length() == 2)
-                System.out.printf(" %s ", pitches[i]);
-            else
-                System.out.printf(" %s  ", pitches[i]);
-
-            for (int j = 0; j < counts[0].length; j++)
-                System.out.printf(" %d,", counts[i][j]);
-
-            System.out.printf("\n");
+        for (int k = 0; k < order; k++) {
+            System.out.println("-----------------------------------------");
+            markov.get(k).printStateHistogram();
+            System.out.println("-----------------------------------------");
         }
     }
 
     public void printProbMatrix() {
-        normalize();
-
-        System.out.printf("    ");
-        String [] pitches = axis.getPitchAxis();
-        for (String s : pitches) {
-            if (s.length() == 2)
-                System.out.printf(" %s  ", s);
-            else
-                System.out.printf(" %s   ", s);
-        }
-
-        System.out.printf("\n");
-
-        for (int i = 0; i < probMatrix.length; i++) {
-            if (pitches[i].length() == 2)
-                System.out.printf(" %s  ", pitches[i]);
-            else
-                System.out.printf(" %s   ", pitches[i]);
-
-            for (int j = 0; j < probMatrix[0].length; j++) {
-                System.out.printf("%.2f,", probMatrix[i][j]);
-            }
-            System.out.printf("\n");
+        for (int k = 0; k < order; k++) {
+            System.out.println("-----------------------------------------");
+            markov.get(k).printStateProbMatrix();
+            System.out.println("-----------------------------------------");
         }
     }
 }
