98909b5cebb86de54c384a4bfd818bb524afea25
Sean <phill117@purdue.edu>
2015-04-06 20:37:17 -0400

    added methods to get top notes and notes within beat groupings (only for n/4 time as of now)

--- Impromptune/src/data_objects/MetaData.java
+++ Impromptune/src/data_objects/MetaData.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class MetaData {
 
     int tempo = 120;
-    int divisions = 8;
+    int divisions = 8; // divisions per quarter note (unit for duration)
     int sharps = 0;
     boolean major = true;
     int beats = 4;
@@ -93,5 +93,67 @@ public class MetaData {
 
     public ArrayList<Measure> getMeasures() {
         return measures;
+    }
+
+    //gets the first note of every chord (Impromptune only accepts one note at a time so this is default) and puts it in a list
+    public ArrayList<Note> getNoteList(){
+        ArrayList notes = new ArrayList<>();
+
+        for(Measure measure : measures){
+            for(ArrayList<Note> chords : measure.getChords()){
+                notes.add(chords.get(0));
+            }
+        }
+
+        return notes;
+    }
+
+    public ArrayList<ArrayList<Note>> getBeatList(){
+
+        ArrayList<Note> noteList = getNoteList();
+        ArrayList<ArrayList<Note>> beats = new ArrayList<>();
+
+
+        int currentDuration = 0;
+        ArrayList<Note> currentBeat = null;
+        int leftover = 0;
+
+        if(beattype == 4){
+            for(Note currentNote : noteList){
+                if(currentDuration == 0 || leftover > 0){
+                    //if got overfilled...
+                    if(leftover > 0){
+                        currentDuration = leftover;
+                        leftover = 0;
+                    }
+                    //create a new beat set if the last one gets filled
+                    if(currentBeat != null) beats.add(currentBeat);
+                    currentBeat = new ArrayList<>();
+                }
+
+                //add the current note to the current beat
+                currentBeat.add(currentNote);
+                //and add that notes duration to the running total
+                currentDuration += currentNote.getDuration();
+
+                //if the running total exceeds the number of possible divisions...
+                if(currentDuration > divisions){
+                    //cache the last note's duration
+                    int currentNoteDuration = currentNote.getDuration();
+                    //loop through to add whole beats in case the note has a beat larger than the division (whole note in 4/4)
+                    while(currentNoteDuration > divisions){
+                        currentNoteDuration -= divisions;
+                        beats.add(currentBeat);
+                        currentBeat = new ArrayList<>();
+                        currentBeat.add(currentNote);
+                    }
+                    leftover = currentDuration % divisions; //or leftover = currentNoteDuration;
+                }
+            }
+        }else{ // its n/8 time
+
+        }
+
+        return beats;
     }
 }
