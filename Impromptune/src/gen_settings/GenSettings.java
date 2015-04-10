bd42f78b5c1f00e4d9a35f185ca6b98b06755771
Jacob Richwine <jrichwin@purdue.edu>
2015-04-09 23:43:29 -0400

    generator bugfix

--- Impromptune/src/gen_settings/GenSettings.java
+++ Impromptune/src/gen_settings/GenSettings.java
@@ -89,12 +89,20 @@ public class GenSettings implements Initializable, EventHandler<ActionEvent> {
 
         //save current file to xml
         FileChooser chooser = new FileChooser();
+
+        File custom = new File(".");
+        chooser.setInitialDirectory(custom);
+
         chooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("XML", "*.xml"),
                 new FileChooser.ExtensionFilter("MusicXML", "*.mxl")
         );
         chooser.setTitle("Save Composition as MusicXML");
         File file = chooser.showSaveDialog(stage);
+
+        if(file == null)
+            return;
+
         mainWindow.saveAs(file);
 
 
