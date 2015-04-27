dfff2a7d9f87bfc90ac314bcc575dcc309a69268
Sean <phill117@purdue.edu>
2015-04-27 01:09:03 -0400

    added accidental support

--- Impromptune/src/xml_parser/MXMLWriter.java
+++ Impromptune/src/xml_parser/MXMLWriter.java
@@ -158,6 +158,10 @@ public class MXMLWriter {
                             writer.writeStartElement("step");
                             writer.writeCharacters(Character.toString(note.getPitch()).toUpperCase());
                             writer.writeEndElement();
+                            //alter
+                            writer.writeStartElement("alter");
+                            writer.writeCharacters(Integer.toString(note.getAccidental()));
+                            writer.writeEndElement();
                             //octave
                             writer.writeStartElement("octave");
                             writer.writeCharacters(Integer.toString(note.getOctave()));
