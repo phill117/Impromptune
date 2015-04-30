3c0e148257edd3a0ee416289641a87c4f1d8e2e2
Sean <phill117@purdue.edu>
2015-04-29 21:22:37 -0400

    commented out shit

--- Impromptune/src/gen_settings/GenSettings.java
+++ Impromptune/src/gen_settings/GenSettings.java
@@ -168,11 +168,12 @@ public class GenSettings implements Initializable, EventHandler<ActionEvent> {
                     reader.parse(inputSource);
 
                     //increment divisions
-                    ArrayList<Measure> measures = MetaData.getInstance().getMeasures();
-                    int currentDivisions = MetaData.getInstance().getDivisions();
-                    while(currentDivisions != 32){
-                        for(Measure measure : measures) for(Note note : measure.getPart(0)) note.setDuration(note.getDuration()*2);
-                    }
+//                    ArrayList<Measure> measures = MetaData.getInstance().getMeasures();
+//                    int currentDivisions = MetaData.getInstance().getDivisions();
+//                    while(currentDivisions < 32){
+//                        for(Measure measure : measures) for(Note note : measure.getPart(0)) note.setDuration(note.getDuration()*2);
+//                        currentDivisions*=2;
+//                    }
 
                     int tehOrder = Double.valueOf(order_scroll.getValue()).intValue();
                     int tehVoicez = Double.valueOf(voices_scroll.getValue()).intValue();
